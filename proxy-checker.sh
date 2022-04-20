f88ebedc9736410cf0bbc74371b7d7ca5078387b
abhishek.pandey <abhishek.pandey@grasshopperasia.com>
2022-04-20 19:47:50 +0800

    update scripts

--- proxy-checker.sh
+++ proxy-checker.sh
@@ -9,12 +9,12 @@ then
     echo -e "starting app" >> /tmp/proxy-checker.log
     echo -e "------------------" >> /tmp/proxy.log
     echo -e "starting app" >> /tmp/proxy.log
-    nohup /home/abhishek.pandey/code/crypto/freqtrade-proxy/dist/freqtrade-proxy -port 6001 -verbose 1 >> /tmp/proxy.log 2>&1 &
+    nohup /home/abhishek.pandey/code/crypto/freqtrade-proxy/dist/exchange-proxy -port 6001 -verbose 1 >> /tmp/proxy.log 2>&1 &
 else
     echo -e "App is already running" >> /tmp/proxy-checker.log
 fi
 }
 
-ID=$( ps -ef | egrep dist.freqtrade.proxy | ag -v grep  | awk '{print $2}')
+ID=$( ps -ef | egrep dist.exchange.proxy | ag -v grep  | awk '{print $2}')
 startup_proxy $ID
 
