282c8dc607a1bec8f02c47419adf74a80cce11fb
Abhishek Pandey <abhi.pandey@gmail.com>
2023-02-11 18:29:38 +0800

    update paths

--- freqtrade.proxy.sh
+++ freqtrade.proxy.sh
@@ -10,5 +10,5 @@ fi
 
 ID=$( ps -ef | egrep dist.freqtrade.proxy | ag -v grep  | awk '{print $2}')
 kill_by_id $ID
-nohup /home/abhishek.pandey/code/crypto/freqtrade-proxy/dist/freqtrade-proxy -port 6001 -verbose 1 > /tmp/proxy.log 2>&1 &
+nohup /Users/abhi/code/crypto/freqtrade-proxy/dist/freqtrade-proxy -port 6001 -verbose 1 > /tmp/proxy.log 2>&1 &
 
--- proxy-checker.sh
+++ proxy-checker.sh
@@ -9,7 +9,7 @@ then
     echo -e "starting app" >> /tmp/proxy-checker.log
     echo -e "------------------" >> /tmp/proxy.log
     echo -e "starting app" >> /tmp/proxy.log
-    nohup /home/abhishek.pandey/code/crypto/freqtrade-proxy/dist/exchange-proxy -port 6001 -verbose 1 >> /tmp/proxy.log 2>&1 &
+    nohup /Users/abhi/code/crypto/freqtrade-proxy/dist/exchange-proxy -port 6001 -verbose 1 >> /tmp/proxy.log 2>&1 &
 else
     echo -e "App is already running" >> /tmp/proxy-checker.log
 fi
